6f07be09fda3a86013d6daf0be753064d4abeb5a
AM Hoffman <avamariehoffman@gmail.com>
2022-03-07 12:49:35 -0500

    Try again now that creds are good

--- scripts/anvil_repo_check.R
+++ scripts/anvil_repo_check.R
@@ -2,9 +2,9 @@
 
 library(optparse)
 library(httr)
-# library(jsonlite)
-# library(dplyr)
-# library(readr)
+library(jsonlite)
+library(dplyr)
+library(readr)
 
 option_list <- list(
   optparse::make_option(
@@ -40,30 +40,26 @@ req <- httr::GET(
   httr::add_headers(Authorization = paste("token", git_pat))
 )
 
+if (!(httr::http_error(req))) {
+  message(paste("API request successful!"))
+  repo_dat <-
+    jsonlite::fromJSON(httr::content(req, as = "text"), flatten = TRUE)
+  repo_df <-
+    dplyr::tibble(repo_dat$items) %>% dplyr::select(name, html_url)
+  if (!dir.exists("resources")) {
+    dir.create("resources")
+  }
+  if (nrow(repo_df) > 0) {
+    write(nrow(repo_df), stdout())
+    readr::write_tsv(repo_df, file.path('resources', 'AnVIL_repos.tsv'))
+  } else {
+    readr::write_tsv(tibble(), file.path('resources', 'AnVIL_repos.tsv'))
+  }
 
-message(paste(httr::content(req, as = "text")))
-message(paste("http_error:", httr::http_error(req)))
-
-# if (!(httr::http_error(req))) {
-#   message(paste("API request successful!"))
-#   repo_dat <-
-#     jsonlite::fromJSON(httr::content(req, as = "text"), flatten = TRUE)
-#   repo_df <-
-#     dplyr::tibble(repo_dat$items) %>% dplyr::select(name, html_url)
-#   if (!dir.exists("resources")) {
-#     dir.create("resources")
-#   }
-#   if (nrow(repo_df) > 0) {
-#     write(nrow(repo_df), stdout())
-#     readr::write_tsv(repo_df, file.path('resources', 'AnVIL_repos.tsv'))
-#   } else {
-#     readr::write_tsv(tibble(), file.path('resources', 'AnVIL_repos.tsv'))
-#   }
-#
-# } else {
-#   message(paste("API request failed!"))
-#   if (!dir.exists("resources")) {
-#     dir.create("resources")
-#   }
-#   readr::write_tsv(tibble(), file.path('resources', 'AnVIL_repos.tsv'))
-# }
+} else {
+  message(paste("API request failed!"))
+  if (!dir.exists("resources")) {
+    dir.create("resources")
+  }
+  readr::write_tsv(tibble(), file.path('resources', 'AnVIL_repos.tsv'))
+}
