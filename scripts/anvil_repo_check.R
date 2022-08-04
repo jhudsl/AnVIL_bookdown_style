6f08e0508dfcc1aba809cd1715eb90a9af67a819
avahoffman <avamariehoffman@gmail.com>
2022-08-04 12:57:07 -0400

    Update query and content

--- scripts/anvil_repo_check.R
+++ scripts/anvil_repo_check.R
@@ -20,23 +20,31 @@ opt_parser <- optparse::OptionParser(option_list = option_list)
 opt <- optparse::parse_args(opt_parser)
 git_pat <- opt$git_pat
 
-message(paste("Checking for AnVIL repositories..."))
+message(paste("Checking for repositories..."))
 
 # Request search results specific to AnVIL within the jhudsl organization
 # and provide the appropriate GH token
 req <- httr::GET(
-  "https://api.github.com/search/repositories?q=GDSCN+user:jhudsl+OR+AnVIL+user:jhudsl",
+  "https://api.github.com/search/repositories?q=user:jhudsl&per_page=500",
   httr::add_headers(Authorization = paste("token", git_pat))
 )
 
 if (!(httr::http_error(req))) {
   message(paste("API request successful!"))
-  
+
   # Read in and save data
   repo_dat <-
     jsonlite::fromJSON(httr::content(req, as = "text"), flatten = TRUE)
   repo_df <-
-    dplyr::tibble(repo_dat$items) %>% dplyr::select(name, html_url)
+    dplyr::tibble(repo_dat$items) %>%
+    dplyr::select(name, homepage, html_url) %>%
+    dplyr::bind_cols(tibble(anvil = unlist(
+      lapply(repo_dat$items$topics, function(x) {
+        "anvil" %in% x
+      })
+    ))) %>% dplyr::bind_cols(tibble(topics = unlist(
+      lapply(repo_dat$items$topics, paste, collapse = ", ")
+    ))) %>% filter(anvil, !(is.na(homepage)))
   
   # Create an artifact file containing the AnVIL repos, else write an empty file
   if (!dir.exists("resources")) {
