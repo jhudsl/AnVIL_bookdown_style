2a96382bce6f2115b6cdf51d7dd1db9d9b0d03f4
avahoffman <avamariehoffman@gmail.com>
2023-07-13 15:54:49 -0400

    Add printing for people's sanity

--- scripts/AnVIL_Feedback_Script.sh
+++ scripts/AnVIL_Feedback_Script.sh
@@ -12,4 +12,8 @@ title=${title// /+}
 # Base url for the AnVIL Google Form
 url_base="https://docs.google.com/forms/d/e/1FAIpQLScrDVb_utm55pmb_SHx-RgELTEbCCWdLea0T3IzS0Oj00GE4w/viewform?usp=pp_url&entry.1565230805="
 
+# Replace the url inside _output.yml
 perl -i -pe 's|https://docs.google.com/forms/d/e/1FAIpQLScrDVb_utm55pmb_SHx-RgELTEbCCWdLea0T3IzS0Oj00GE4w/viewform\?usp=pp_url&entry\.1565230805=.*"> Click|'${url_base}${title}'"> Click|g' _output.yml
+
+# Print for Github actions 
+echo "The feedback link is: ${url_base}${title}"
